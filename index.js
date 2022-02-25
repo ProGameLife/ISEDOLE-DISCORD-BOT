27ad0364c9d04a6ef30ff596f196743347ee3542
감돌이 <rnsdls0823@naver.com>
2022-02-25 19:42:41 +0900

    memory leak issue

--- index.js
+++ index.js
@@ -25,13 +25,16 @@ client.on('ready', async () => {
     const channel = await client.channels.fetch(process.env.CHANNEL_ID);
     
     cron.schedule('*/1 * * * *', async () => {
+        let browser;
+        let page;
         try{
-            const browser = await puppeteer.launch({
+            browser = await puppeteer.launch({
                 headless: true,
                 args: ['--no-sandbox', '--disable-setuid-sandbox']
             });
+            page = await browser.newPage();
+        
 
-            const page = await browser.newPage();
             await page.goto('https://cafe.naver.com/steamindiegame');
             await page.click("#menuLink331");
             
@@ -68,32 +71,36 @@ client.on('ready', async () => {
             
             if(linkchek != cilplink[0]){
                 const embed = new MessageEmbed()
-                .setColor('#0099ff')
-                .setTitle('🎬 이세돌 | 핫클립')
-                .setURL('https://cafe.naver.com/steamindiegame/#menuLink331')
-                .setAuthor({ name: '이세돌사랑해', iconURL: 'https://yt3.ggpht.com/5vwZ3NZL6Zv4C7cl5sshsTk-XycH7r-4zo6nQR7g9Z7SLrMzeabWWzn5M1V3SqJXjTxLj_hb=s88-c-k-c0x00ffffff-no-rj', url: 'https://cafe.naver.com/steamindiegame/#menuLink331' })
-                .setDescription('이세돌 많은 사랑 부탁드립니다!')
-                .setThumbnail('https://w.namu.la/s/e65c5665ec17d3de4f8afe1681e270aec2f2d0c477083d049a11857f523dec07f65c0d619c288f6018fc717a3b1983ee280a1966c08f3717e8d19cd6c9764f4c07376f1dcc0002de24abf7d4cc12495a946d225b169f55d62a32da31c11a1bb820d1a409534d64a7169a6d849ed9450a')
-                .addFields(
-                    { name: '제목', value: titlename[0] },
-                    { name: '카페 글링크', value: process.env.MAINLINK + geturl[0], inline: true },
-                )
-                .setImage('https://w.namu.la/s/f99dbf4ce7f02fc9b75572f63ea0352d1548ae7e7b04a95fe0541f2d8462be10c0cea4aedc206705b9b52b411436aa3afd08e102f59f8683cfa5a59626b2baa0e811169ac0c7ed772fc4e9a6aa95b5a09ae09e56b0aac2f076cf67e936ab0af9')
-                .setTimestamp()
-                .setFooter({ text: '⬇️⬇️⬇️⬇️⬇️⬇️⬇️영상 맛보기⬇️⬇️⬇️⬇️⬇️⬇️⬇️'});
+                    .setColor('#0099ff')
+                    .setTitle('🎬 이세돌 | 핫클립')
+                    .setURL('https://cafe.naver.com/steamindiegame/#menuLink331')
+                    .setAuthor({ name: '이세돌사랑해', iconURL: 'https://yt3.ggpht.com/5vwZ3NZL6Zv4C7cl5sshsTk-XycH7r-4zo6nQR7g9Z7SLrMzeabWWzn5M1V3SqJXjTxLj_hb=s88-c-k-c0x00ffffff-no-rj', url: 'https://cafe.naver.com/steamindiegame/#menuLink331' })
+                    .setDescription('이세돌 많은 사랑 부탁드립니다!')
+                    .setThumbnail('https://w.namu.la/s/e65c5665ec17d3de4f8afe1681e270aec2f2d0c477083d049a11857f523dec07f65c0d619c288f6018fc717a3b1983ee280a1966c08f3717e8d19cd6c9764f4c07376f1dcc0002de24abf7d4cc12495a946d225b169f55d62a32da31c11a1bb820d1a409534d64a7169a6d849ed9450a')
+                    .addFields(
+                        { name: '제목', value: titlename[0] },
+                        { name: '카페 글링크', value: process.env.MAINLINK + geturl[0], inline: true },
+                    )
+                    .setImage('https://w.namu.la/s/f99dbf4ce7f02fc9b75572f63ea0352d1548ae7e7b04a95fe0541f2d8462be10c0cea4aedc206705b9b52b411436aa3afd08e102f59f8683cfa5a59626b2baa0e811169ac0c7ed772fc4e9a6aa95b5a09ae09e56b0aac2f076cf67e936ab0af9')
+                    .setTimestamp()
+                    .setFooter({ text: '⬇️⬇️⬇️⬇️⬇️⬇️⬇️영상 맛보기⬇️⬇️⬇️⬇️⬇️⬇️⬇️'});
                 
                 channel.send({ embeds: [embed] });
                 linkchek = cilplink[0];
                 if(cilplink[0] && cilplink[0].startsWith(process.env.STARTLINK)){
-                channel.send(cilplink[0]);
+                    channel.send(cilplink[0]);
                 }
             }
-            await page.close();
-            await browser.close();
         } catch (e){
             console.log(e);
             Sentry.captureException(e);
         } finally {
+            if(page){
+                await page.close();
+            }
+            if(browser){
+                await browser.close();
+            }
             transaction.finish();
         }
     });
